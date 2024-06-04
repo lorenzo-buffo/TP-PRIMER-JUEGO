8295bbec063e6af3949b60cc968c9bf39df67728
lorenzo-buffo <lolitobuffo@gmail.com>
2024-06-04 16:13:14 -0300

    aparicion de bomba

--- scenes/End.js
+++ scenes/End.js
@@ -6,10 +6,11 @@ export default class End extends Phaser.Scene {
     init(data) {
         console.log("data", data)
         this.score = data.score || 0;
-        this.gameOver = data.gameOver
-
+        this.gameOver = data.gameOver 
         console.log (this.gameOver)
     }
+
+    
 
     create(){
         
@@ -23,8 +24,16 @@ export default class End extends Phaser.Scene {
         .setOrigin(0.5);
         
         this.add.text(400, 350, `Score ${this.score}`);
+        this.r = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     }
+  
+    update() {
+      if (this.r.isDown) {
+        this.scene.start("main");
+      }
+    }
+  }
+    
       
     
 
-}
--- scenes/Game.js
+++ scenes/Game.js
@@ -1,199 +1,247 @@
-// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
 
-export default class Game extends Phaser.Scene {
-  constructor() {
-    super("main");
-  }
-
-  init() {
-    this.gameOver = false;
-    this.timer = 20;
-    this.score = 0;
-    this.shapes = {
-      "triangle": {point: 10, count: 0},
-      "square": {point: 20, count: 0},
-      "diamond": {point: 30, count:0}
+  export default class Game extends Phaser.Scene {
+    constructor() {
+      super("main");
     }
-  }
-
-  preload() {
-  //cargar assets
-
-  //import Cielo
-    this.load.image("Cielo", "../public/assets/Cielo.webp");
-
-    //import plataforma
-    this.load.image("platform", "../public/assets/platform.png");
-
-    //import pj
-    this.load.image("personaje", "../public/assets/Ninja.png")
-
-    //import recolectable
-    this.load.image("triangle", "../public/assets/triangle.png")
-    this.load.image("square", "../public/assets/square.png")
-    this.load.image("diamond", "../public/assets/diamond.png")
-
-    
-}
-  create() {
-  //Crear elemnto
-
-  this.Cielo = this.add.image(400, 300, "Cielo");
-  this.Cielo.setScale(2)
-
-  //Crear pltaforma
-  this.platforms = this.physics.add.staticGroup();
-
-  //al grupo de plataformas agregar plataforma
-  this.platforms.create(400, 565, "platform").setScale(2).refreshBody();
-
-  this.platforms.create(200, 400, "platform")
-
-  //Crear pj
-  this.personaje = this.physics.add.sprite(400, 300, "personaje")
-  this.personaje.setScale(0.1);
-  this.personaje.setCollideWorldBounds(true);
-
-  //agregar colision entre pj y plataforma
-  this.physics.add.collider(this.personaje, this.platforms);
-
-  //crear teclas
-  this.cursor = this.input.keyboard.createCursorKeys(); 
-
-  //una tecla a la vez
-  //this.w = this.input.kayboard.addKey(Phaser.Input.Keyboard.keyCodes.W);
-
-  //crear grupo recolectables
-  this.recolectables = this.physics.add.group(); 
-  this.physics.add.collider(this.recolectables, this.recolectables)
-
-  this.physics.add.collider(this.personaje, this.recolectables, this.pj, null, this)
   
-  this.physics.add.collider(this.platforms, this.recolectables, this.bounce, null, this)
-
-
-  //add tecla r
-  this.r = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-
-  //evento 1 segundo
-  this.time.addEvent({
-    delay: 1000,
-    callback: this.onSecond,
-    callbackScope: this,
-    loop: true,
-  });
-
-  //agregar texto de timer en la esquina superior derecha
-
-  this.timerText = this.add.text(10, 10, `tiempo restante: ${this.timer}`, {
-
-    fontSize: "32px",
-
-    fill: "#fff"
-
-  });
-
-
-  //timer de 1 seg
-  this.time.addEvent({ 
-    delay: 1000,
-    callback: this.handlerTimer, 
-    callbackScope: this, 
-    loop: true,
-  })
-
-  this.scoreText = this.add.text(10, 50, `puntaje: ${this.score}
-  / T: ${this.shapes["triangle"].count}
-  / S: ${this.shapes["square"].count}
-  / D: ${this.shapes["diamond"].count}`)
-
-}
-
-pj(personaje, recolectables){
-  const nombreFig = recolectables.texture.key;
-  const puntoFig = this.shapes[nombreFig].point;
-  this.score += puntoFig;
-  this.shapes[nombreFig].count += 1;
-  console.table(this.shapes);
-  console.log("score", this.score);
-  console.log("recolectado");
-  recolectables.destroy();
-
-  this.scoreText.setText(
-    `puntaje: ${this.score}
+    init() {
+      this.gameOver = false;
+      this.timer = 20;
+      this.score = 0;
+      this.shapes = {
+        "triangle": {point: 10, count: 0},
+        "square": {point: 20, count: 0},
+        "diamond": {point: 30, count:0},
+        "bomb": {  point: -10, count:0},
+      };
+      
+  }
+     
+    
+  
+    preload() {
+    //cargar assets
+  
+    //import Cielo
+      this.load.image("Cielo", "../public/assets/Cielo.webp");
+  
+      //import plataforma
+      this.load.image("platform", "../public/assets/platform.png");
+  
+      //import pj
+      this.load.image("personaje", "../public/assets/Ninja.png")
+  
+      //import recolectable
+      this.load.image("triangle", "../public/assets/triangle.png")
+      this.load.image("square", "../public/assets/square.png")
+      this.load.image("diamond", "../public/assets/diamond.png")
+      this.load.image("bomb", "../public/assets/bomb.png")
+  
+      
+  }
+  create() {
+    //Crear elemnto
+  
+    this.Cielo = this.add.image(400, 300, "Cielo");
+    this.Cielo.setScale(2)
+  
+    //Crear pltaforma
+    this.platforms = this.physics.add.staticGroup();
+  
+    //al grupo de plataformas agregar plataforma
+    this.platforms.create(400, 565, "platform").setScale(2).refreshBody();
+  
+    this.platforms.create(200, 400, "platform")
+  
+    //Crear pj
+    this.personaje = this.physics.add.sprite(400, 300, "personaje")
+    this.personaje.setScale(0.1);
+    this.personaje.setCollideWorldBounds(true);
+  
+    //agregar colision entre pj y plataforma
+    this.physics.add.collider(this.personaje, this.platforms);
+  
+    //crear teclas
+    this.cursor = this.input.keyboard.createCursorKeys(); 
+  
+    //una tecla a la vez
+    //this.w = this.input.kayboard.addKey(Phaser.Input.Keyboard.keyCodes.W);
+  
+    //crear grupo recolectables
+    this.recolectables = this.physics.add.group(); 
+    this.physics.add.collider(this.recolectables, this.recolectables)
+  
+    this.physics.add.collider(this.personaje, this.recolectables, this.pj, null, this)
+    
+    this.physics.add.collider(this.platforms, this.recolectables, this.onRecolectableBounced, null, this)
+  
+  
+    //add tecla r
+    this.r = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+  
+    //evento 1 segundo
+    this.time.addEvent({
+      delay: 700,
+      callback: this.onSecond,
+      callbackScope: this,
+      loop: true,
+    });
+  
+    //agregar texto de timer en la esquina superior derecha
+  
+    this.timerText = this.add.text(10, 10, `tiempo restante: ${this.timer}`, {
+  
+      fontSize: "32px",
+  
+      fill: "#fff"
+  
+    });
+  
+  
+    //timer de 1 seg
+    this.time.addEvent({ 
+      delay: 1000,
+      callback: this.handlerTimer, 
+      callbackScope: this, 
+      loop: true,
+    })
+  
+    this.scoreText = this.add.text(10, 50, `puntaje: ${this.score}
     / T: ${this.shapes["triangle"].count}
     / S: ${this.shapes["square"].count}
-    / D: ${this.shapes["diamond"].count}`
-  );
+    / D: ${this.shapes["diamond"].count}`)
 
-  const cumplePuntos = this.score >= 100;
-  const cumpleFiguras =
+    const cumplePuntos = this.score >= 100;
+    const cumpleFiguras =
+      this.shapes["triangle"].count >= 2 &&
+      this.shapes["square"].count >= 2 &&
+      this.shapes["diamond"].count >= 2;
+
+    if (cumplePuntos % cumpleFiguras) {
+      console.log("Ganaste");
+      this.scene.start("end", {
+        score: this.score,
+        gameOver: this.gameOver
+      })}
+  
+  }
+
+  pj(personaje, recolectables){
+    const nombreFig = recolectables.getData("tipo");
+    const puntosFig = recolectables.getData("points");
+
+    this.score += puntosFig;
+
+    this.shapes [nombreFig].count += 1;
+    
+   // const points= recolectables.getData("points")
+    
+    console.table(this.shapes);
+    console.log("score", this.score);
+    recolectables.destroy();
+
+    this.scoreText.setText(
+      `puntaje: ${this.score} 
+       T: ${this.shapes["triangle"].count} 
+       S: ${this.shapes["square"].count}  
+       D: ${this.shapes["diamond"].count}`
+    )
+     
+    this.checkWin();
+  }
+
+
+  checkWin(){
+    const cumplePuntos = this.score >= 100;
+    const cumpleFiguras = 
     this.shapes["triangle"].count >= 2 &&
     this.shapes["square"].count >= 2 &&
     this.shapes["diamond"].count >= 2;
 
-  if(cumplePuntos && cumpleFiguras) {
-    console.log("Ganaste");
-    this.scene.start("end",{score:this.score, gameOver: this.gameOver})
+    if (cumplePuntos && cumpleFiguras) {
+      console.log("Ganaste");
+      this.scene.start("end",{
+        score: this.score,
+        gameOver: this.gameOver,
+      })
+     }
   }
-}
+  
 
-floor(platforms, recolectables){
-  recolectables.disableBody(true,true)
-  }
-
-  onSecond() {
-    //crear reecolectable
+  
+  floor(platforms, recolectables){
+    recolectables.disableBody(true,true)
+    }
+  
+    onSecond() {
+      //crear reecolectable
+      
+  
+      const tipos = ["triangle", "square", "diamond", "bomb"];
+      const tipo = Phaser.Math.RND.pick(tipos);
+  
+      let recolectable = this.recolectables.create(
+        Phaser.Math.Between(10, 790),
+        0,
+        tipo
+      );
+      recolectable.setVelocity(0, 100);
+  
+      const rebote = Phaser.Math.FloatBetween(0.4, 0.8);
+      recolectable.setBounceY(rebote);
+      this.physics.add.collider(recolectable, this.recolectables)
+       //set data
+    recolectable.setData("points",this.shapes[tipo].points);
+    recolectable.setData("tipo",tipo);
+    }
+    onRecolectableBounced(recolectables, _plataformas){
+      let points = recolectables.getData("points");
+      points -= 5;
+      recolectables.setData("points", points);
+      if (points <= 0) {
+        recolectables.destroy();
+      }
+      console.log(this.onRecolectableBounced)
+     }
+  
+    handlerTimer() {
+      this.timer -= 1;
+      this.timerText.setText(`tiempo restante: ${this.timer}`);
+      if (this.timer === 0) {
+        this.gameOver = true;
+        this.gameOver = true;
+        this.scene.start("end",{
+          score: this.score,
+          gameOver: this.gameOver,
+        })
+      } 
+     
+    }
+      
     
-
-    const tipos = ["triangle", "square", "diamond"]
-    const tipo = Phaser.Math.RND.pick(tipos);
-
-    let recolectable = this.recolectables.create(
-      Phaser.Math.Between(10, 790),
-      0,
-      tipo
-    );
-    recolectable.setVelocity(0, 100);
-
-    const rebote = Phaser.Math.FloatBetween(0.4, 0.8);
-    recolectable.setBounceY(rebote);
-    this.physics.add.collider(recolectable, this.recolectables)
-  }
-
-  handlerTimer() {
-    this.timer -= 1;
-    this.timerText.setText(`tiempo restante: ${this.timer}`);
-    if (this.timer === 0) {
-      this.gameOver = true;
-    }
-  }
-
+    update() {
+      if (this.cursor.left.isDown) {
+        this.personaje.setVelocityX(-160)
+      } else if (this.cursor.right.isDown) {
+        this.personaje.setVelocityX(160)
+      } else this.personaje.setVelocityX(0) 
   
-  update() {
-    if (this.gameOver) { 
-      // this.scene.pause();
-      // this. timerText.setText("Game Over");
-      // return;
-      this.scene.start("end",{score:this.score,gameOver:this.gameOver})
-    }
-    //movimiento
-    if (this.cursor.left.isDown) {
-      this.personaje.setVelocityX(-160);
-    } 
-    else if (this.cursor.right.isDown) {
-      this.personaje.setVelocityX(160);
-    }
-    else {
-      this.personaje.setVelocityX(0);
-    } 
-
-    if (this.cursor.up.isDown && this.personaje.body.touching.down) {
-      this.personaje.setVelocityY(-330)
-    }
+      if (this.cursor.up.isDown && this.personaje.body.touching.down){
+        this.personaje.setVelocityY(-330); 
+      }
+      if( this.r.isDown){
+        console.log("reincia")
+        this.scene.restart(`GAME`);
+        
+      }
+      if(this.gameOver){
+          this.physics.pause();
+          this.timerText.setText("Game Over");
+          return;
+      }
+      
+    }  
+    
   }
-
-
-  // HOLA COMO VA
-}
-  
+    
+   
